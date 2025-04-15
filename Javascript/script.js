ac92f2379e53c56cb5a3bd9f69c7055cf9174415
Muhammad Faizan <imuhammadfaizanpk@gmail.com>
2025-04-15 09:28:20 +0500

    15-04-25

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,156 @@
-let a = "Hello World!";
+// let a = "Hello World!";
+
+
+// let ab = 5;
+//  ab++;
+//  let z = ab;
+// document.write(ab);
+// function bt(){
+
+   
+//     alert("click is runing with number"  )
+
+// }
+
+// let btn=document.getElementById("btn");
+// btn.addEventListener("contextmenu" , bt )
+
+
+// let arr = ["<h1>HTML</h1>", "<h1>CSS</h1>", "<h1>BootStrap</h1>", "<h1>Javascript</h1>"];
+//     document.write(arr);
+
+// let arr1 = ["<h1>Pakistan</h1>", "<h1>South Korea</h1>", "<h1>USA</h1>"]
+
+// let arr2 = ["<h1>Islamabad</h1>", "<h1>Seoul</h1>", "<h1>Los Angles</h1>"]
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+// SPLICE & SLICE
+
+// let arr=["HTML", "CSS", "BootStrap", "Javascript"];
+
+// let newarr=arr.splice(1,2, "PHP", "Laravel", "MySQL",);
+// console.log(newarr);
+// console.log(arr);
+
+
+// let arr=["HTML", "CSS", "BootStrap", "Javascript"];
+
+// let newarr=arr.toSpliced(1,1, "PHP", "Laravel", "MySQL",);
+// console.log(newarr);
+// console.log(arr);
+
+// let x = "Pakistan"
+// if (x == "Pakistan") {
+//    alert("Your Condition is True");
+    
+// }
+// else{
+//     alert("Your condition is False");
+// }
+
+// let y = 20;
+
+// if (y<=20) {
+//     document.write("Condition is True")
+    
+// }
+// else{
+//     document.write("Condition is False")
+// }
+
+// let y = prompt("Enter a number");
+
+// if (y<=20) {
+//     document.write("Condition is True")
+    
+// }
+// else{
+//     document.write("Condition is False")
+// }
+
+
+// let d = prompt("Enter a Number");
+
+// if (d % 2==0 || d % 3==0) {
+//     document.write("Your Number is Divisible by 2",)
+// }
+// else{
+//     document.write("Your Number is Divisible by 3")
+// }
+
+
+// let username = prompt("Enter Username");
+// let password = Number(prompt("Enter Password"));
+
+// if (username === "admin" && password === 12345) {
+//     document.write("Access Granted!")
+    
+// }
+// else{
+//     document.write("<h1>Access Denied!</h1>");
+// }
+
+// let value1 = Number(prompt("Enter Value"));
+// let value2 = Number(prompt("Enter Value"));
+// let value3 = Number(prompt("Enter Value"));
+// let avg = ( value1 + value2 + value3) / 3 ;
+// document.write("Average is " + avg);
+
+// for largest value 
+// let j = Number(prompt("Enter to get largest Number"));
+// let k = Number(prompt("Enter to get largest Number"));
+// let l = Number(prompt("Enter to get largest Number"));
+
+// if (j>k && j>l){
+//     document.write(j + "<br>");
+// }
+// if (k>l && k>j){
+//     document.write(k + "<br>");
+// }
+// else{
+//     document.write(l + "<br>");
+// }
+// for smallest value 
+// let o = Number(prompt("Enter to get smallest  Number"));
+// let p = Number(prompt("Enter to get smallest  Number"));
+// let q = Number(prompt("Enter to get smallest Number"));
+
+// if (o<p && o<q){
+//     document.write(o + "<br>");
+// }
+// if (p<o && p<q){
+//     document.write(p + "<br>");
+// }
+// else{
+//     document.write(q);
+// }
+
+// let day = Number(prompt("Enter a Number"));
+// switch (day) {
+//     case 1:
+//         document.write("Monday");
+//         break;
+//     case 2:
+//         document.write("Tuesday");
+//         break;
+//     case 3:
+//         document.write("Wednesday");
+//         break;
+//     case 4:
+//         document.write("Thursday");
+//         break;
+//     case 5:
+//         document.write("Friday");
+//         break;
+//     case 6:
+//         document.write("Saturday");
+//         break;
+//     case 7:
+//         document.write("Sunday");
+//         break;
+//     default:
+//         document.write("It's Not a Weekday");
+// }  
